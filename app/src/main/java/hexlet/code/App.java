dab23e6f0e8d6ed033b3bc65fdefe81d84eaf2ae
Kirill Vorozhtsov <razer5559@ya.ru>
2024-09-15 19:45:14 +0400

    UserName

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,8 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
         var scan = new Cli();
-        System.out.print("May I have your name?");
-        var name = scan.userName();
+        System.out.print("May I have your name? ");
+         var name = scan.userName();
         System.out.println("Hello, " + name + "!");
     }
 }
