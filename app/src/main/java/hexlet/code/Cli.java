b9662e0830d7a21cbaaec55176a31c88e7fbe20a
Kirill Vorozhtsov <razer5559@ya.ru>
2024-09-17 18:22:55 +0400

    redact Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,13 +2,12 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public final class  Cli {
-    private static String name = "";
+public class  Cli {
     public static void startProgramm() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name? ");
         var scanner = new Scanner(System.in);
-        name = scanner.next();
+        var name = scanner.next();
         System.out.println("Hello, " + name + "!");
     }
 }
