1131b3b107181398aa3bc9372e87406aff66f3e0
Kirill Vorozhtsov <razer5559@ya.ru>
2024-09-24 23:34:14 +0400

    redact Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,24 +4,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Progression {
-    private static final int MaxRandomNumbers = 100;
-    private static final int MaxNumbers = 10;;
-    private static final int MaxDifference = 5;
-    private static final int MaxPosition = 9;
+    private static final int max_random_numbers = 100;
+    private static final int max_numbers = 10;;
+    private static final int max_difference = 5;
+    private static final int max_position = 9;
 
     public static boolean progression() {
 
-        int[] numbers = new int[MaxNumbers];
+        int[] numbers = new int[max_numbers];
         var scanner = new Scanner(System.in);
         var random = new Random();
-        var number = random.nextInt(1, MaxRandomNumbers);
+        var number = random.nextInt(1, max_random_numbers);
         numbers[0] = number;
-        var randomCount = random.nextInt(1, MaxDifference);
+        var randomCount = random.nextInt(1, max_difference);
         System.out.print("Question: ");
         for (var j = 1; j < numbers.length; j++) {
             numbers[j] = numbers[j - 1] + randomCount;
         }
-        var randomPosition = random.nextInt(MaxPosition);
+        var randomPosition = random.nextInt(max_position);
         for (int j = 0; j < numbers.length; j++) {
             if (j == randomPosition) {
                 System.out.print(".. ");
