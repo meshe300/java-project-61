ce251aa9f933847e8a2ee42546df6f618acabce6
Kirill Vorozhtsov <razer5559@ya.ru>
2024-10-25 20:13:39 +0400

    redact Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,44 +1,45 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Progression {
     private static final int MAX_RANDOM = 100;
     private static final int MAX_NUMBERS = 10;;
     private static final int MAX_DIFFERENCE = 5;
     private static final int MAX_POSITION = 9;
+    private static final int COUNT = 3;
+    private  static final String RULE = "What number is missing in the progression?";
 
-    public static boolean progression() {
-
-        int[] numbers = new int[MAX_NUMBERS];
-        var scanner = new Scanner(System.in);
+    public static void progression() {
         var random = new Random();
-        var number = random.nextInt(1, MAX_RANDOM);
-        numbers[0] = number;
-        var randomCount = random.nextInt(1, MAX_DIFFERENCE);
-        System.out.print("Question: ");
-        for (var j = 1; j < numbers.length; j++) {
-            numbers[j] = numbers[j - 1] + randomCount;
-        }
-        var randomPosition = random.nextInt(MAX_POSITION);
-        for (int j = 0; j < numbers.length; j++) {
-            if (j == randomPosition) {
-                System.out.print(".. ");
-                continue;
+        String[][] questionAndAnswer = new String[COUNT][2];
+        String answer;
+        for (int i = 0; i < COUNT; i++) {
+            String question = "";
+            int[] numbers = new int[MAX_NUMBERS];
+            String[] skipList = new String[MAX_NUMBERS];
+            var number = random.nextInt(1, MAX_RANDOM);
+            numbers[0] = number;
+            var randomCount = random.nextInt(1, MAX_DIFFERENCE);
+            var randomPosition = random.nextInt(MAX_POSITION);
+            for (var j = 1; j < numbers.length; j++) {
+                numbers[j] = numbers[j - 1] + randomCount;
             }
-            System.out.print(numbers[j] + " ");
+            for (int j = 0; j < skipList.length; j++) {
+                skipList[j] = Integer.toString(numbers[j]);
+                if (j == randomPosition) {
+                    skipList[j] = "..";
+                }
+                question += skipList[j] + " ";
+            }
+            answer = Integer.toString(game(numbers, randomPosition));
+            questionAndAnswer[i][0] = question;
+            questionAndAnswer[i][1] = answer;
         }
-        System.out.println();
-        int answer = scanner.nextInt();
-        System.out.println("Your answer: " + answer);
-        if (answer == numbers[randomPosition]) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + numbers[randomPosition]
-                    + "'.");
-            return false;
-        }
+        Engine.engine(RULE,questionAndAnswer);
+    }
+    public static int game(int[] numbers, int position) {
+        return  numbers[position];
     }
 }
