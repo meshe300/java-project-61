9ee4fdf51fab8b3f9679e8f0520fa9f53efa86ff
Kirill Vorozhtsov <razer5559@ya.ru>
2024-10-25 18:41:55 +0400

    redact Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,87 +1,34 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Even;
 import java.util.Scanner;
 
 public class Engine {
     private static final int COUNT = 3;
-    private static final int GAME_EVEN = 2;
-    private static final int GAME_CALC = 3;
-    private static final int GAME_GCD = 4;
-    private static final int GAME_PROGRESSION = 5;
-    private static final int GAME_PRIME = 6;
 
-    public static void game(int numberGame) {
-        var scanner = new Scanner(System.in);
+    public static void engine(String gameRule, String[][] questionAndAnswer) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        var name = scanner.next();
+        String name = scan.next();
         System.out.println("Hello, " + name + "!");
-        var count = 0;
+        System.out.println(gameRule);
+        boolean result = true;
         for (int i = 0; i < COUNT; i++) {
-            if (numberGame == GAME_EVEN) {
-                if (i == 0) {
-                    System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-                }
-                var res2  = Even.even();
-                if (res2) {
-                    count++;
-                } else {
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (numberGame == GAME_CALC) {
-                if (i == 0) {
-                    System.out.println("What is the result of the expression?");
-                }
-                var res3  = Calc.calc();
-                if (res3) {
-                    count++;
-                } else {
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (numberGame == GAME_GCD) {
-                if (i == 0) {
-                    System.out.println("Find the greatest common divisor of given numbers.");
-                }
-                var res4  = GCD.gcd();
-                if (res4) {
-                    count++;
-                } else {
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (numberGame == GAME_PROGRESSION) {
-                if (i == 0) {
-                    System.out.println("What number is missing in the progression?");
-                }
-                var res5  = Progression.progression();
-                if (res5) {
-                    count++;
-                } else {
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (numberGame == GAME_PRIME) {
-                if (i == 0) {
-                    System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-                }
-                var res6  = Prime.prime();
-                if (res6) {
-                    count++;
-                } else {
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
+            System.out.println("Question: " + questionAndAnswer[i][0]);
+            System.out.print("Your answer: ");
+            String answer = scan.next();
+            if (answer.equals(questionAndAnswer[i][1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println(answer + " is wrong answer ;(. Correct answer was " + questionAndAnswer[i][1] + ".");
+                result = false;
+                break;
             }
         }
-        if (count == COUNT) {
+        if (result) {
             System.out.println("Congratulations, " + name + "!");
+        } else {
+            System.out.println("Let's try again, " + name + "!");
         }
     }
 }
