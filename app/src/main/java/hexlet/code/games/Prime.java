d17977f3685d25c2d927f04627d1f835aab6c639
Kirill Vorozhtsov <razer5559@ya.ru>
2024-10-25 20:31:54 +0400

    redact Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,38 +1,36 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Prime {
     private static final int MAX_RANDOM = 100;
+    private static final int COUNT = 3;
+    private  static final String RULE = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
-    public static boolean prime() {
-        var scanner = new Scanner(System.in);
+    public static void prime() {
         var randomNumber = new Random();
-        var number = randomNumber.nextInt(2, MAX_RANDOM);
-        System.out.println("Question: " + number);
-        var answer = scanner.next();
-        System.out.println("Your answer: " + answer);
-        var res = false;
+        String[][] questionAndAnswer = new String[COUNT][2];
+        String question;
+        String answer;
+        for (int i = 0; i < COUNT; i++) {
+            var number = randomNumber.nextInt(2, MAX_RANDOM);
+            question = Integer.toString(number);
+            questionAndAnswer[i][0] = question;
+            answer = game(number);
+            questionAndAnswer[i][1] = answer;
+        }
+        Engine.engine(RULE,questionAndAnswer);
+    }
+    public static String game(int number) {
+        String answer = "yes";
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                res = true;
+                answer = "no";
                 break;
             }
         }
-        if (!res && answer.equals("yes")) {
-            System.out.println("Correct!");
-            return true;
-        } else if (res && answer.equals("no")) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            if (answer.equals("yes")) {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'.");
-            } else if (answer.equals("no")) {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'.");
-            }
-            return false;
-        }
+        return answer;
     }
 }
