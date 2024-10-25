e777896945de63075db1684f0bb6e793e336e844
Kirill Vorozhtsov <razer5559@ya.ru>
2024-10-25 19:22:37 +0400

    redact GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,23 +1,37 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class GCD {
     private static final int MAX_RANDOM = 100;
+    private static final int COUNT = 3;
+    private  static final String RULE = "Find the greatest common divisor of given numbers.";
 
-    public static boolean gcd() {
+    public static void gcd() {
         var random = new Random();
-        var scanner = new Scanner(System.in);
-        var numberFirst = random.nextInt(1, MAX_RANDOM);
-        var numberSecond = random.nextInt(1, MAX_RANDOM);
-        var res = 1;
+        String[][] questionAndAnswer = new String[COUNT][2];
+        String question;
+        String answer;
+        for (int i = 0; i < COUNT; i++) {
+            var numberFirst = random.nextInt(1, MAX_RANDOM);
+            var numberSecond = random.nextInt(1, MAX_RANDOM);
+            question = numberFirst + " " + numberSecond;
+            questionAndAnswer[i][0] = question;
+            answer = Integer.toString(game(numberFirst, numberSecond));
+            questionAndAnswer[i][1] = answer;
+        }
+        Engine.engine(RULE,questionAndAnswer);
+    }
+    public static int game(int numberFirst, int numberSecond) {
+        var result = 1;
         if (numberFirst > numberSecond) {
             for (int j = numberSecond; j > 1; j--) {
                 if ((numberSecond % j) == 0 && (numberFirst % j) == 0) {
-                    res = j;
-                    if (res == 0) {
-                        res += 1;
+                    result = j;
+                    if (result == 0) {
+                        result += 1;
                     }
                     break;
                 }
@@ -25,25 +39,16 @@ public class GCD {
         } else if (numberFirst < numberSecond) {
             for (int j = numberFirst; j > 1; j--) {
                 if ((numberSecond % j) == 0 && (numberFirst % j) == 0) {
-                    res = j;
-                    if (res == 0) {
-                        res += 1;
+                    result = j;
+                    if (result == 0) {
+                        result += 1;
                     }
                     break;
                 }
             }
         } else if (numberFirst == numberSecond) {
-            res = numberFirst;
+            result = numberFirst;
         }
-        System.out.println("Question: " + numberFirst + " " + numberSecond);
-        var answer = scanner.nextInt();
-        System.out.println("Your answer: " + answer);
-        if (res == answer) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + res + "'.");
-            return false;
-        }
+        return result;
     }
 }
